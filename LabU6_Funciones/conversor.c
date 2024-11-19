6b7962488cd423c8a084338dc4ccfd9551956af4
Oalonso11 <calinalonso00@outlook.com>
2024-11-19 17:57:25 -0600

    Update conversor.c

--- LabU6_Funciones/conversor.c
+++ LabU6_Funciones/conversor.c
@@ -1,60 +1,67 @@
 /* Creado por Oscar, Viktor y Sebas.
- Programa en lenguaje C que permita elegir el siguiente cambio de divisas:
- 1.-Pesos a Dólares
- 2.-Dólares a Euros
- 3.-Euros a Pesos*/
+ Programa en lenguaje C que permite elegir el siguiente cambio de divisas:
+ 1.- Pesos a Dólares
+ 2.- Dólares a Euros
+ 3.- Euros a Pesos
+*/
 #include <stdio.h>
 #include <stdlib.h>
 //Usamos la función para seleccionar el proceso de cambio de divisas
-void menu(int *opcion){
+void menu(int *opcion) {
     printf("Seleccione una opcion:\n");
     printf("1. Pesos a Dólares\n");
     printf("2. Dólares a Euros\n");
     printf("3. Euros a Pesos\n");
     scanf("%d", opcion);
 }
-float Convertir_Pesos_Dolares(){
+float Convertir_Pesos_Dolares() {
     float pesos, dolares;
     printf("\nIngrese la cantidad en pesos que quiere convertir a dólares: ");
-    scanf("%f", &pesos);
-    dolares = pesos*.050;
+    scanf("%f",&pesos);
+    dolares = pesos*0.050;
     return dolares;
 }
-float Convertir_Dolares_Euros(){
-    float dolares,euros;
-    printf("\nIngrese la cantidad en dolares que quiere convertir a euros: ");
-    scanf("%f", &dolares);
-    euros = dolares*.94;
+float Convertir_Dolares_Euros() {
+    float dolares, euros;
+    printf("\nIngrese la cantidad en dólares que quiere convertir a euros: ");
+    scanf("%f",&dolares);
+    euros = dolares*0.94;
     return euros;
 }
-float Convertir_Euros_Pesos(){
-    float euros,pesos;
+float Convertir_Euros_Pesos() {
+    float euros, pesos;
     printf("\nIngrese la cantidad en euros que quiere convertir a pesos: ");
-    scanf("%f", &euros);
+    scanf("%f",&euros);
     pesos = euros*21.30;
     return pesos;
 }
-int main(){
+int main() {
     int opcion;
     float resultado;
-    menu(&opcion); //Llamamos la función menu
-    switch(opcion){
-        case 1:
+    menu(&opcion); // Llamamos la función menu
+    switch(opcion) {
+        case 1: {
             resultado = Convertir_Pesos_Dolares();
-            printf("La cantidad de %f pesos equivale a: %.2f\n dolares",pesos, resultado);
+            printf("La cantidad ingresada: %.2f pesos\n", resultado / 0.050); //Esto se hacae para poder sacar la cantidad sin convertirla
+            printf("Equivale a: %.2f dólares\n", resultado);
             break;
-        case 2:
+        }
+        case 2: {
             resultado = Convertir_Dolares_Euros();
-            printf("La cantidad de %f dolares equivale a: %.2f\n euros", dolares, resultado);
+            printf("La cantidad ingresada: %.2f dólares\n", resultado / 0.94);
+            printf("Equivale a: %.2f euros\n", resultado);
             break;
-        case 3:
+        }
+        case 3: {
             resultado = Convertir_Euros_Pesos();
-            printf("La cantidad de %f euros equivale a: %.2f\n pesos",euros,resultado);
+            printf("La cantidad ingresada: %.2f euros\n", resultado / 21.30); 
+            printf("Equivale a: %.2f pesos\n", resultado);
             break;
+        }
         default:
-            printf("Opcion incorrecta\n");
+            printf("Opción incorrecta\n");
             break;
     }
-    printf("\nPrograma termiando\n");
+    printf("\nPrograma terminado\n");
     return 0;
 }
