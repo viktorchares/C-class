2804307f5a485011f31f0f376f75b00a91e4e699
viktorchares <137464423+viktorchares@users.noreply.github.com>
2024-11-21 08:19:28 -0600

    Comprobar positivos en P2 Conversiones

--- LabU6_Funciones/P2_OscarSebastianViktor.c
+++ LabU6_Funciones/P2_OscarSebastianViktor.c
@@ -17,11 +17,30 @@ void menu(int *opcion) {
     printf("2. Dólares a Euros\n");
     printf("3. Euros a Pesos\n");
     scanf("%d", opcion);
+
+    while(*opcion <1 || 3 < *opcion){
+        printf("Debes ingresar el valor 1, 2 o el 3\n");
+        printf("Seleccione una opcion:\n");
+        printf("1. Pesos a Dólares\n");
+        printf("2. Dólares a Euros\n");
+        printf("3. Euros a Pesos\n");
+        scanf("%d", opcion);
+    }
+}
+
+float comprobarPositivos(float entrada){
+    while(entrada<0){
+        printf("\nError, debes ingresar un entero positivo\n");
+        printf("Ingresa la cantidad: ");
+        scanf("%f",&entrada);
+    }
+    return entrada;
 }
 float Convertir_Pesos_Dolares() {
     float pesos, dolares;
     printf("\nIngrese la cantidad en pesos que quiere convertir a dólares: ");
     scanf("%f",&pesos);
+    pesos=comprobarPositivos(pesos);
     dolares = pesos*0.050;
     return dolares;
 }
@@ -29,6 +48,7 @@ float Convertir_Dolares_Euros() {
     float dolares, euros;
     printf("\nIngrese la cantidad en dólares que quiere convertir a euros: ");
     scanf("%f",&dolares);
+    dolares=comprobarPositivos(dolares);
     euros = dolares*0.94;
     return euros;
 }
@@ -36,6 +56,7 @@ float Convertir_Euros_Pesos() {
     float euros, pesos;
     printf("\nIngrese la cantidad en euros que quiere convertir a pesos: ");
     scanf("%f",&euros);
+    euros=comprobarPositivos(euros);
     pesos = euros*21.30;
     return pesos;
 }
